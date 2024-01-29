57ed62a25eb7e49fd69cab5e4f0b46b12fab9ae1
natwikan <151179668+natxhr@users.noreply.github.com>
2024-01-29 18:24:55 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+require('dotenv').config();
 
 @Module({
   imports: [
-    CustomersModule, 
-    MongooseModule,forRoot(
-      'mongodb+srv://natwikan:kozume3362882@cluster0.vtoyvr5.mongodb.net/ecommercedb?retryWrites=true&w=majority')],
+    CustomersModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
